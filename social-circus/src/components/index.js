139c723107d0b9fdf379f192ae7e6e58725fd2b5
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-05-28 19:59:17 +0530

    Feat : Added Text Area Component

--- social-circus/src/components/index.js
+++ social-circus/src/components/index.js
@@ -1,5 +1,6 @@
 import { FormikControl } from "./FormikControl/FormikControl";
 import { Header } from "./Header/Header";
 import { TextError } from "./TextError/TextError";
+import { TextArea } from "./TextArea/TextArea";
 
-export { Header, FormikControl, TextError };
+export { Header, FormikControl, TextError, TextArea };
