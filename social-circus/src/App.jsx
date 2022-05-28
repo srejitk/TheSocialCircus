817be120c5b5c707abc9bba3f5f07ae11bae1595
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-05-29 00:39:00 +0530

    Styles : Updated Styles

--- social-circus/src/App.jsx
+++ social-circus/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Header } from "./components";
 import { RouteConfig } from "./config/RouteConfig";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <RouteConfig />
     </div>
   );
