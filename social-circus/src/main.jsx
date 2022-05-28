7439829d21eaede35dd4adc69c020cd76ceb4a63
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-05-29 01:38:00 +0530

    Feat : Added Redux Toolkit

--- social-circus/src/main.jsx
+++ social-circus/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+store;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
