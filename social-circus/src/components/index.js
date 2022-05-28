36da610e4fbeba15e50dac603467fe8d37829cee
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-05-28 20:00:11 +0530

    Feat : Added Input Component

--- social-circus/src/components/index.js
+++ social-circus/src/components/index.js
@@ -2,5 +2,6 @@ import { FormikControl } from "./FormikControl/FormikControl";
 import { Header } from "./Header/Header";
 import { TextError } from "./TextError/TextError";
 import { TextArea } from "./TextArea/TextArea";
+import { Input } from "./Input/Input";
 
-export { Header, FormikControl, TextError, TextArea };
+export { FormikControl, Header, Input, TextError, TextArea };
