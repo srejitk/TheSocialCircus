d6c9c4e6ba1070f45741f8fee18477d060fc4068
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-05-28 19:13:35 +0530

    Chore : Refactored path names

--- social-circus/src/config/RouteConfig.jsx
+++ social-circus/src/config/RouteConfig.jsx
@@ -5,12 +5,12 @@ import { Error, Explore, Feed, Home, Login, Profile, Signup } from "../routes";
 export const RouteConfig = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/explore" element={<Explore />} />
-      <Route path="/feed" element={<Feed />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="explore" element={<Explore />} />
+      <Route path="feed" element={<Feed />} />
       <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="profile" element={<Profile />} />
       <Route path="*" element={<Error />} />
     </Routes>
   );
