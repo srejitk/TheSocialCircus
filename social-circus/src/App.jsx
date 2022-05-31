21d3a5846dfe7e5936424cccecf20b43f8b6601d
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-06-01 02:58:55 +0530

    Feat : Added React-hot-toasts

--- social-circus/src/App.jsx
+++ social-circus/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Header } from "./components";
 import { RouteConfig } from "./config/RouteConfig";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <RouteConfig />
+      <Toaster />
     </div>
   );
 }
