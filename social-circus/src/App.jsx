07692df704336b94178e7f8d2da4fcc207a253fa
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-06-03 02:16:43 +0530

    Feat : Added onAuthStateChanged Check

--- social-circus/src/App.jsx
+++ social-circus/src/App.jsx
@@ -1,12 +1,34 @@
 import "./App.css";
-import { Header } from "./components";
+import { CreatePost, Header } from "./components";
 import { RouteConfig } from "./config/RouteConfig";
 import { Toaster } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import { login, logout } from "./redux/slice/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (userCreds) => {
+      if (userCreds) {
+        dispatch(
+          login({
+            email: userCreds.email,
+            uid: userCreds.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  });
   return (
     <div className="App">
       <Header />
+      <CreatePost />
       <RouteConfig />
       <Toaster />
     </div>
