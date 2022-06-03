948446252c4c507bf92d430e8590ed2fa4d39be5
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-06-04 03:51:21 +0530

    Feat : Added Firebase Storage

--- social-circus/src/firebase.jsx
+++ social-circus/src/firebase.jsx
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_APIKEY,
@@ -15,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { app, auth, db };
+export { app, auth, db, storage };
