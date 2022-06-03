bc467efba6c4e444521f507d8ed7458badd3e257
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-06-04 03:56:08 +0530

    Feat : Added Postcards to listing

--- social-circus/src/routes/Explore/Explore.jsx
+++ social-circus/src/routes/Explore/Explore.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { PostCard } from "../../components";
 
 export const Explore = () => {
-  return <div>Explore</div>;
+  const { posts } = useSelector((state) => state.post);
+
+  return (
+    <div>
+      {posts?.length}
+      {posts.map((post) => {
+        return <PostCard post={post} />;
+      })}
+    </div>
+  );
 };
