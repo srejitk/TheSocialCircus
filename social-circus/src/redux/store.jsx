3638cfdd33078f96cc04994b1a404b5810666693
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-06-06 03:41:59 +0530

    Fix : serializableCheck

--- social-circus/src/redux/store.jsx
+++ social-circus/src/redux/store.jsx
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
 import postReducer from "./slice/postSlice";
+import thunk from "redux-thunk";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     post: postReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  thunk,
 });
