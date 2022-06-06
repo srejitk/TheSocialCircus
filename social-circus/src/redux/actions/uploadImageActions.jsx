cd362e23efa0001afce51518c290e39dcc05a580
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-06-06 22:40:45 +0530

    Feat : Added Image/Avatar Upload Calls

--- social-circus/src/redux/actions/uploadImageActions.jsx
+++ social-circus/src/redux/actions/uploadImageActions.jsx
@@ -1,27 +1,30 @@
+import { toast } from "react-hot-toast";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage } from "../../firebase";
 
-export const UploadImage = async (folder, file) => {
-  const storageRef = ref(storage, `${folder}/${file.name}`);
-  const uploadTask = await uploadBytesResumable(storageRef, file);
-  const path = uploadTask?.ref?.toString();
-  console.log(path);
-  //Please ignore the below comments || WIP
-  //   const link = await DownloadImage(path);
-  //   console.log(link);
-  //   return link;
-
-  //   const imageRef = ref(storage, `posts/${id.uid}/post-cover.jpg`);
-  //   const response = await uploadBytes(imageRef, image);
-  //   const picRef = ref(storage, `posts/${id.uid}/post-cover.jpg`);
-  //   const imageResponse = await getMetadata(picRef);
-  //   setImagePath(imageResponse);
+export const UploadImage = async (path, file) => {
+  try {
+    const imageRef = ref(storage, path);
+    const response = await uploadBytesResumable(imageRef, file);
+    const pathName = response?.ref?.toString();
+    const gsReference = ref(storage, pathName);
+    const url = await getDownloadURL(gsReference);
+    return url;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const DownloadImage = async (path) => {
-  let link = "";
-  const response = await getDownloadURL(ref(storage, path));
-  const { url } = response;
-  link = url;
-  return link;
+export const UploadAvatar = async (path, file) => {
+  try {
+    const imageRef = ref(storage, path);
+    const response = await uploadBytesResumable(imageRef, file);
+    const pathName = response?.ref?.toString();
+    const gsReference = ref(storage, pathName);
+    const url = await getDownloadURL(gsReference);
+    return url;
+  } catch (error) {
+    console.log(error);
+    toast.error("Could'nt upload Avatar");
+  }
 };
