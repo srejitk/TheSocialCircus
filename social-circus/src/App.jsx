eb784a05e1da3590c90b1699348770982e4f96da
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-06-06 22:59:16 +0530

    Feat : Added Conditional rendering

---
+++ social-circus/src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { Header, Sidebar } from "./components";
+import { RouteConfig } from "./config/RouteConfig";
+import { Toaster } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getExplorePosts } from "./redux/actions/postActions";
+import { getUserData } from "./redux/actions/authActions";
+import { useLocation } from "react-router-dom";
+
+function App() {
+  const dispatch = useDispatch();
+  const token = localStorage.getItem("userID");
+
+  useEffect(() => {
+    dispatch(getExplorePosts());
+    dispatch(getUserData(token));
+  }, [token]);
+  const { pathname } = useLocation();
+
+  return (
+    <div className="App grid grid-cols-3 gap-4">
+      {pathname === "/signup" ||
+      pathname === "/login" ||
+      pathname === "/update" ? null : (
+        <Header />
+      )}
+      {pathname === "/signup" ||
+      pathname === "/login" ||
+      pathname === "/update" ? null : (
+        <Sidebar />
+      )}
+      <RouteConfig />
+      <Toaster />
+    </div>
+  );
+}
+
+export default App;
