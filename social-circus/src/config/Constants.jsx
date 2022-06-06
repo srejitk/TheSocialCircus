09d36b0c91124de55160b9c9163f95bc349394d9
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-06-06 22:59:34 +0530

    Feat : Added Avatar Constants

---
+++ social-circus/src/config/Constants.jsx
@@ -0,0 +1,22 @@
+export const avatarOptions = [
+  {
+    id: 1,
+    avatar:
+      "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1654526304/The%20Social%20Circus/Avatars/otsaiilart-06062022-0001_x106ua.jpg",
+  },
+  {
+    id: 2,
+    avatar:
+      "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1654526358/The%20Social%20Circus/Avatars/otsaiilart-06062022-0002_ov5nb2.jpg",
+  },
+  {
+    id: 3,
+    avatar:
+      "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1654526357/The%20Social%20Circus/Avatars/otsaiilart-06062022-0003_ntzg9w.jpg",
+  },
+  {
+    id: 4,
+    avatar:
+      "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1654526356/The%20Social%20Circus/Avatars/otsaiilart-06062022-0004_fwb03u.jpg",
+  },
+];
