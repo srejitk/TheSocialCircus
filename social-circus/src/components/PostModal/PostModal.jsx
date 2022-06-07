4eb3483edb6df15a701a8349c398f31929104104
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-06-07 12:39:32 +0530

    Refactor : Destructured Props

--- social-circus/src/components/PostModal/PostModal.jsx
+++ social-circus/src/components/PostModal/PostModal.jsx
@@ -21,12 +21,14 @@ export const PostModal = ({ openModal, setOpenModal, edit }) => {
     uid: "",
     likes: [],
     comments: [],
+    avatar: "",
+    username: "",
   };
 
   const dispatch = useDispatch();
 
-  const { token } = useSelector((state) => state.auth);
-  const { post } = useSelector((state) => state.post);
+  const { user, token } = useSelector((state) => state.auth);
+  const { posts, post } = useSelector((state) => state.post);
   const [selectedEmoji, setSelectedEmoji] = useState(null);
   const [imagePath, setImagePath] = useState("");
   const [form, setForm] = useState(initialValues);
@@ -53,6 +55,8 @@ export const PostModal = ({ openModal, setOpenModal, edit }) => {
       displayName: auth.currentUser.displayName,
       uid: token,
       imageUrl: imagePath,
+      username: user?.username,
+      avatar: user?.avatar,
       date: edit ? post?.data?.date : new Date().toLocaleString(),
     });
     setSelectedEmoji(null);
@@ -60,7 +64,12 @@ export const PostModal = ({ openModal, setOpenModal, edit }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setForm({ ...form, imageUrl: imagePath });
+    setForm({
+      ...form,
+      imageUrl: imagePath,
+      username: user?.username,
+      avatar: user?.avatar,
+    });
     if (form.content !== "") {
       edit
         ? await EditPost(form, dispatch, post?.id)
@@ -71,15 +80,9 @@ export const PostModal = ({ openModal, setOpenModal, edit }) => {
     dispatch(getExplorePosts());
   };
 
-  const handleImage = async (image) => {
-    if (image === null) return;
-    UploadImage(`posts/${token}`, image);
-    //Please ignore the below code || WIP
-    // const imageRef = ref(storage, `posts/${id.uid}/post-cover.jpg`);
-    // const response = await uploadBytes(imageRef, image);
-    // const picRef = ref(storage, `posts/${id.uid}/post-cover.jpg`);
-    // const imageResponse = await getMetadata(picRef);
-    // setImagePath(imageResponse);
+  const handleImage = async (file) => {
+    const link = await UploadImage(`users/${token}/user-avatar.jpg`, file);
+    setImagePath(link);
   };
 
   const emojiClickHandler = (e) => {
@@ -92,81 +95,79 @@ export const PostModal = ({ openModal, setOpenModal, edit }) => {
   };
 
   return (
-    <Modal openModal={openModal} setOpenModal={setOpenModal}>
-      <div className="w-100 relative mx-3 my-3  flex h-72 flex-col items-start justify-between gap-4 rounded-lg border-2 border-gray-100 bg-white px-3 py-6 shadow-md">
-        <form
-          onSubmit={(e) => handleSubmit(e)}
-          className="relative flex h-full w-full flex-col items-start justify-between"
-        >
-          {edit ? (
-            <button
-              onClick={(e) => closeModal(e)}
-              className="absolute right-1 top-1 rounded-full border-2 border-transparent bg-white p-3 hover:border-red-50 hover:bg-red-50"
-            >
-              <FiX />
-            </button>
-          ) : null}
+    <div className="relative mx-3 my-3 flex  h-72 w-full flex-col items-start justify-between gap-4 rounded-lg border-2 border-gray-100 bg-white px-3 py-6 shadow-md">
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        className="relative flex h-full w-full flex-col items-start justify-between"
+      >
+        {edit ? (
+          <button
+            onClick={(e) => closeModal(e)}
+            className="absolute right-1 top-1 rounded-full border-2 border-transparent bg-white p-3 hover:border-red-50 hover:bg-red-50"
+          >
+            <FiX />
+          </button>
+        ) : null}
 
-          <div className="align-center flex w-full justify-between">
-            <div className="w-14 pt-3">
-              <img
-                src="https://64.media.tumblr.com/75320ca1fcab8631c111abca8bf055a4/fdcf9f0f117edd2e-7b/s250x250_c1/2a81402340070baae8e3eae599d3916facc19fe1.png"
-                alt="profile of user"
-                className="w-full rounded-full"
-              />
-            </div>
-            <input
-              type="textarea"
-              name="newPost"
-              value={form.content}
-              onChange={(e) => handleChange(e)}
-              placeholder="What's on your mind?"
-              className=" h-24 w-full flex-wrap rounded-xl border-2 border-transparent px-3 focus:bg-gray-50 focus:outline-none"
+        <div className="align-center flex w-full justify-between">
+          <div className="w-14 pt-3">
+            <img
+              src={user?.avatar}
+              alt="profile of user"
+              className="w-full rounded-full"
             />
           </div>
-          <div className="relative flex w-full flex-row items-center gap-4 ">
-            <div className="group rounded-3xl border-2 border-gray-100 bg-white p-4 hover:border-2 hover:border-blue-300 hover:bg-blue-50 hover:outline-2">
-              <label htmlFor="upload-picture">
-                <input
-                  type="file"
-                  id="upload-picture"
-                  accept="image/*"
-                  className="hidden"
-                  onChange={(e) => {
-                    handleImage(e.target.files[0]);
-                  }}
-                />
-                <FiImage className="group-hover:font-bold group-hover:text-blue-500 " />
-              </label>
-            </div>
-            <button
-              onClick={(e) => emojiClickHandler(e)}
-              className={`group rounded-3xl border-2 border-gray-100 bg-white p-4 hover:border-2 hover:border-blue-300 hover:bg-blue-50 hover:outline-2 ${
-                openEmoji ? "border-blue-300 bg-blue-50" : ""
-              }`}
-            >
-              <FiSmile className="group-hover:font-bold group-hover:text-blue-500 " />
-            </button>
-
-            {openEmoji && (
-              <div className="absolute left-16 -bottom-72">
-                <Picker
-                  onEmojiClick={onEmojiClick}
-                  disableSearchBar={true}
-                  pickerStyle={{
-                    height: "15rem",
-                    width: "25rem",
-                    borderRadius: "10px",
-                  }}
-                />
-              </div>
-            )}
-            <button className="m-left my-3 flex w-1/4 items-center justify-center gap-3 rounded-xl bg-blue-500  py-2 font-semibold text-white hover:bg-blue-600">
-              {edit ? "Update Post" : "Post"}
-            </button>
+          <input
+            type="textarea"
+            name="newPost"
+            value={form.content}
+            onChange={(e) => handleChange(e)}
+            placeholder={`What's on your mind ${user?.firstname}?`}
+            className=" h-24 w-full flex-wrap rounded-xl border-2 border-transparent px-3 focus:bg-gray-50 focus:outline-none"
+          />
+        </div>
+        <div className="relative flex w-full flex-row items-center gap-4 ">
+          <div className="group rounded-3xl border-2 border-gray-100 bg-white p-4 hover:border-2 hover:border-blue-300 hover:bg-blue-50 hover:outline-2">
+            <label htmlFor="upload-picture">
+              <input
+                type="file"
+                id="upload-picture"
+                accept="image/*"
+                className="hidden"
+                onChange={(e) => {
+                  handleImage(e.target.files[0]);
+                }}
+              />
+              <FiImage className="group-hover:font-bold group-hover:text-blue-500 " />
+            </label>
           </div>
-        </form>
-      </div>
-    </Modal>
+          <button
+            onClick={(e) => emojiClickHandler(e)}
+            className={`group rounded-3xl border-2 border-gray-100 bg-white p-4 hover:border-2 hover:border-blue-300 hover:bg-blue-50 hover:outline-2 ${
+              openEmoji ? "border-blue-300 bg-blue-50" : ""
+            }`}
+          >
+            <FiSmile className="group-hover:font-bold group-hover:text-blue-500 " />
+          </button>
+
+          {openEmoji && (
+            <div className="absolute left-16 -bottom-72">
+              <Picker
+                onEmojiClick={onEmojiClick}
+                disableSearchBar={true}
+                pickerStyle={{
+                  height: "15rem",
+                  width: "25rem",
+                  borderRadius: "10px",
+                }}
+              />
+            </div>
+          )}
+          <button className="m-left my-3 flex w-1/4 items-center justify-center gap-3 rounded-xl bg-blue-500  py-2 font-semibold text-white hover:bg-blue-600">
+            {edit ? "Update Post" : "Post"}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
