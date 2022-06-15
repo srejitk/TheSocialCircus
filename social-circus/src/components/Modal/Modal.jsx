0a92168edbb211be1edb08afa5bc9e2e014fba08
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-06-15 23:09:13 +0530

    Feat : Added Overlay

--- social-circus/src/components/Modal/Modal.jsx
+++ social-circus/src/components/Modal/Modal.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom";
 
 export const Modal = ({ openModal, setOpenModal, children }) => {
   return ReactDOM.createPortal(
-    openModal ? (
-      <div className=" fixed top-1/2 left-1/2 z-30 w-full -translate-x-1/2 translate-y-1/2  ">
-        {children}
-      </div>
-    ) : null,
+    <div className="fixed inset-0 z-50" onClick={(e) => setOpenModal(false)}>
+      <div className="fixed inset-0 bg-slate-500/50"></div>
+      {openModal ? (
+        <div
+          className=" fixed top-1/2 left-1/2  w-[30rem] -translate-x-1/2 -translate-y-1/2"
+          onClick={(e) => e.stopPropagation()}
+        >
+          {children}
+        </div>
+      ) : null}
+    </div>,
     document.getElementById("portal")
   );
 };
