668623f0d68667d6950af34c991ee423fcf2d989
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-07-01 03:18:30 +0530

    Feat : Added Landing Page

--- social-circus/src/config/RouteConfig.jsx
+++ social-circus/src/config/RouteConfig.jsx
@@ -10,6 +10,7 @@ import {
   Update,
   User,
   Explore,
+  Landing,
 } from "../routes";
 import { PrivateRoutes } from "./PrivateRoutes";
 
@@ -18,11 +19,13 @@ export const RouteConfig = () => {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={<Landing />} />
       <Route element={<PrivateRoutes />}>
         <Route path="/update" element={<Update />} />
         <Route path="/saved" element={<Saved />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+
         <Route path="/profile" element={<Profile />} />
         <Route path="/profile/:userID" element={<User />} />
         <Route path="*" element={<Error />} />
---
+++ social-circus/src/routes/Landing/Landing.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+export const Landing = () => {
+  return (
+    <div className="sm:content relative flex h-fit min-h-screen w-full flex-col justify-center bg-white py-[10rem] md:absolute  md:flex-col md:py-[10rem] lg:absolute lg:flex-row lg:items-center lg:py-0">
+      <div className="flex h-full w-full flex-col items-center justify-center  lg:w-3/5">
+        <h2 className=" break-after-auto text-6xl font-black lg:px-4 ">
+          Get the best of both <span className="text-pink-600">Instagram</span>{" "}
+          and <span className="text-blue-500">Twitter</span> at one place.
+        </h2>
+        <Link
+          to="/login"
+          className="align-center flex items-center gap-2 pt-6 text-6xl font-black text-blue-700 transition-[gap] hover:gap-8"
+        >
+          Try for free <p>{">"}</p>
+        </Link>
+      </div>
+      <div className="img mt-20 w-full overflow-hidden rounded-2xl px-4 md:mx-auto md:h-[40rem] md:w-[40rem] lg:h-[40rem] lg:w-[40rem] lg:p-8">
+        <video
+          loop={true}
+          autoPlay={true}
+          className="w-full rounded-xl"
+          src="https://res.cloudinary.com/dkqrmlxlg/video/upload/v1656623194/The%20Social%20Circus/Videos/Graphic-Design-Trends-2020-3D-style-and-animation-example-1_zsznev.mp4"
+        />
+      </div>
+      <h className="absolute left-10 -z-10 w-1/2 text-left text-9xl font-black text-slate-50">
+        WELCOME TO THE CIRCUS
+      </h>
+    </div>
+  );
+};
