d98b1436f1ac6455ada7135fe654849813f8b9d9
Sreejith K <srejitk@gmail.com>
2022-09-29 00:49:11 +0530

    Fix : Unecessary navigation

--- social-circus/src/components/ContactCard/ContactCard.jsx
+++ social-circus/src/components/ContactCard/ContactCard.jsx
@@ -13,21 +13,21 @@ export const ContactCard = ({ id }) => {
   const isFollowing = user?.following?.some((ID) => ID === id);
 
   return (
-    <Link
-      to={`/profile/${otherUser?.id}`}
-      className=" mb-4 flex justify-between border-b-2"
-    >
-      <div className="m-3 h-12 w-12">
+    <div className=" mb-4 flex justify-between border-b-2">
+      <Link to={`/profile/${otherUser?.id}`} className="m-3 h-12 w-12">
         <img
           src={otherUser?.data?.avatar || defaultAvatar}
           alt="contact dp"
           className="rounded-full"
         />
-      </div>
+      </Link>
       <div className="flex flex-grow flex-col items-center justify-start">
-        <p className="flex w-full flex-grow items-center text-left text-base font-semibold">
+        <Link
+          to={`/profile/${otherUser?.id}`}
+          className="flex w-full flex-grow items-center text-left text-base font-semibold"
+        >
           {otherUser?.data?.displayName}
-        </p>
+        </Link>
         <p className="my-auto flex w-full flex-grow items-start text-left text-base font-medium text-gray-500">
           {otherUser?.data?.username === "username"
             ? " @" + otherUser?.data?.toLowerCase()
@@ -37,30 +37,32 @@ export const ContactCard = ({ id }) => {
       <div className="w-fit pr-4">
         {isFollowing ? (
           <button
-            onClick={(e) =>
+            onClick={(e) => {
+              e.stopPropagation();
               unfollowUser(
                 user,
                 otherUser?.data,
                 otherUser?.id,
                 token,
                 dispatch
-              )
-            }
+              );
+            }}
             className="my-3 ml-auto rounded-full border-2 border-blue-600 bg-blue-50 py-3 px-3  font-semibold text-blue-600 hover:bg-blue-600/50"
           >
             <FiUserCheck className="text-blue-600" />
           </button>
         ) : (
           <button
-            onClick={(e) =>
-              followUser(user, otherUser?.data, otherUser?.id, token, dispatch)
-            }
+            onClick={(e) => {
+              e.stopPropagation();
+              followUser(user, otherUser?.data, otherUser?.id, token, dispatch);
+            }}
             className="my-3 ml-auto rounded-3xl bg-blue-500 py-3 px-3  font-semibold text-white hover:bg-blue-600"
           >
             <FiUserPlus className="text-white" />
           </button>
         )}
       </div>
-    </Link>
+    </div>
   );
 };
